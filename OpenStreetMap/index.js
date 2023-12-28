8523a68c89a86c4e8bc4efb574e5dacfb94d54c3
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-28 22:48:01 +0100

    to=do

--- OpenStreetMap/index.js
+++ OpenStreetMap/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
 };
 
 app.use(cors({
-  origin: 'https://localhost:3000/',
+  origin: 'https://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
