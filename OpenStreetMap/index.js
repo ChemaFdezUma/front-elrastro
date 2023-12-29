49c446a2e198a84c4678d2807038d88d6c1030f8
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-29 12:59:18 +0100

    cambios

--- OpenStreetMap/index.js
+++ OpenStreetMap/index.js
@@ -10,7 +10,7 @@ const corsOptions = {
 };
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://el-rastro-nine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
